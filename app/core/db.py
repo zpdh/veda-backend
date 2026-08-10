12b3d8ced12933e445201f163be97d5b3569a933
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-10 02:28:57 -0300

    feat: add unit of work

--- app/core/db.py
+++ app/core/db.py
@@ -1,5 +1,6 @@
 from collections.abc import AsyncGenerator
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -18,3 +19,20 @@ async_session_factory: async_sessionmaker = async_sessionmaker(
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_factory() as session:
         yield session
+
+
+class UnitOfWork:
+    __session: AsyncSession
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.__session = session
+
+    async def commit(self) -> None:
+        await self.__session.commit()
+
+    async def rollback(self) -> None:
+        await self.__session.rollback()
+
+
+async def get_unit_of_work(session: AsyncSession = Depends(get_db)) -> UnitOfWork:  # noqa: B008
+    return UnitOfWork(session)
