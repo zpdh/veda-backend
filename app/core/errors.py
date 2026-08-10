de62a968bbccfa0aee552b21d2dc7d62ee1a73dc
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-09 23:19:11 -0300

    refactor: rewrite error structure

--- app/core/errors.py
+++ app/core/errors.py
@@ -1,15 +1,29 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass(frozen=True)
+class ErrorDescription:
+    status_code: int
+    error_code: str
+
+
+class CommonErrors(Enum):
+    INTERNAL_SERVER_ERROR = ErrorDescription(500, "ERR_INTERNAL")
+
+
 class AppError(Exception):
-    status_code: int = 500
-    error_code: str = "ERR_INTERNAL"
+    status_code: int
+    error_code: str
     message: str
     details: dict | None
 
-    def __init__(self, message: str, details: dict | None = None) -> None:
+    def __init__(
+        self, error_desc: ErrorDescription, message: str, details: dict | None = None
+    ) -> None:
+        self.status_code = error_desc.status_code
+        self.error_code = error_desc.error_code
         self.message = message
-        self.details = details or {}
+        self.details = details
+
         super().__init__(message)
-
-
-class DuplicateRankError(AppError):
-    status_code = 400
-    error_code = "ERR_DUPLICATE_RANK"
--- app/features/leaderboard/errors/errors.py
+++ app/features/leaderboard/errors/errors.py
@@ -1,23 +1,18 @@
 from enum import Enum
 
-from app.core.errors import AppError
+from app.core.errors import AppError, ErrorDescription
 
 
-class LeaderboardErrorType(Enum):
-    DUPLICATE_RANK = (400, "ERR_DUPLICATE_RANK")
+class LeaderboardErrorDescription(ErrorDescription):
+    pass
 
-    def __init__(self, status_code: int, error_code: str) -> None:
-        self.status_code = status_code
-        self.error_code = error_code
+
+class LeaderboardErrors(Enum):
+    DUPLICATE_RANK = LeaderboardErrorDescription(400, "ERR_DUPLICATE_RANK")
 
 
 class LeaderboardError(AppError):
     def __init__(
-        self,
-        error_type: LeaderboardErrorType,
-        message: str,
-        details: dict | None = None,
+        self, error_desc: LeaderboardErrors, message: str, details: dict | None = None
     ) -> None:
-        self.status_code = error_type.status_code
-        self.error_code = error_type.error_code
-        super().__init__(message, details)
+        super().__init__(error_desc.value, message, details)
