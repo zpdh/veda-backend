5a49987e4803f920d4f11161aea45a9234b84ebd
ezlixp <henrygoals2007@gmail.com>
2026-08-29 21:29:38 -0400

    feat: default leaderboards

--- app/features/leaderboard/use_cases/get_config.py
+++ app/features/leaderboard/use_cases/get_config.py
@@ -10,4 +10,11 @@ class GetConfig:
         pass
 
     async def execute(self) -> ConfigResponse:
-        return ConfigResponse(config=[ConfigOut(leaderboardName="Zenith Clears", leaderboardId="Zenith", pages=50)])
+        return ConfigResponse(config=[
+            ConfigOut(leaderboardName="Zenith Clears",
+                      leaderboardId="Zenith", pages=50),
+            ConfigOut(leaderboardName="Silver Knights Tomb",
+                      leaderboardId="SKT", pages=50),
+            ConfigOut(leaderboardName="P.O.R.T.A.L. Strike",
+                      leaderboardId="Portal", pages=50),
+        ])
