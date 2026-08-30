921374c954e319e4f8d65585a98dea4ddcd39d39
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-29 23:23:09 -0300

    feat: add player orm and apply migration

---
+++ app/features/player/api/router.py
@@ -0,0 +1,5 @@
+
+from fastapi import APIRouter
+
+
+player_router = APIRouter(prefix="/v1/api/players", tags=["players"])
--- app/features/user/api/router.py
+++
@@ -1,5 +0,0 @@
-
-from fastapi import APIRouter
-
-
-user_router = APIRouter(prefix="/v1/api/users", tags=["users"])
