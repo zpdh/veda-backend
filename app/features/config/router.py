358031b2a9a0c8f318e451c88bd0dd8acf32a400
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-30 21:57:32 -0300

    fix: fix issues brought by merge

--- app/features/config/router.py
+++ app/features/config/router.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends, Request
 
+from app.core.constants import LIMITER_HTTP_GET
 from app.core.security.rate_limiter import limiter
 from app.features.config.get_config import GetConfig
 from app.features.config.response import ConfigResponse
-from app.features.leaderboard.api.router import _LIMITER_VALUE_GET
 
 config_router = APIRouter(prefix="/v1/api/config", tags=["config"])
 
 
 @config_router.get("/")
-@limiter.limit(_LIMITER_VALUE_GET)
+@limiter.limit(LIMITER_HTTP_GET)
 async def get_config(
     request: Request, use_case: GetConfig = Depends()
 ) -> ConfigResponse:
