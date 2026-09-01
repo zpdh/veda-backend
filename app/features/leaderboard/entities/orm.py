e985acaf44be400c425a3b39ae07d0d7d058f63e
Antunes <36316038+zpdh@users.noreply.github.com>
2026-09-01 05:24:02 -0300

    feat: add more metadata to leaderboard entity

--- app/features/leaderboard/entities/orm.py
+++ app/features/leaderboard/entities/orm.py
@@ -17,9 +17,16 @@ from app.core.db.base import Base
 
 class Leaderboard(Base):
     __tablename__ = "leaderboard"
+    __table_args__ = (
+        CheckConstraint("estimated_time_per_completion_minutes > 0", name="ck_estimated_time_positive"),
+        CheckConstraint("max_player_count > 0", "ck_player_count_positive")
+    )
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    external_leaderboard_id: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
     name: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
+    estimated_time_per_completion_minutes: Mapped[int] = mapped_column(Integer, nullable=False)
+    max_player_count: Mapped[int] = mapped_column(Integer, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
