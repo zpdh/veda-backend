1734dcb7e30c802d140af0b782b6388a943a1941
Antunes <36316038+zpdh@users.noreply.github.com>
2026-09-01 07:04:32 -0300

    refactor: refactor create_snapshot to return 404 if leaderboard is not found

--- app/features/leaderboard/use_cases/create_snapshot.py
+++ app/features/leaderboard/use_cases/create_snapshot.py
@@ -7,9 +7,11 @@ from app.core.db.cache import get_redis
 from app.core.db.unit_of_work import UnitOfWork, get_unit_of_work
 from app.features.leaderboard.dto.request import (
     CreateSnapshotRequest,
+    LeaderboardSnapshotIn,
 )
 from app.features.leaderboard.dto.response import SnapshotCreatedResponse
 from app.features.leaderboard.entities.orm import LeaderboardEntry, LeaderboardSnapshot
+from app.features.leaderboard.errors.errors import LeaderboardError, LeaderboardErrors
 from app.features.leaderboard.repositories.postgres import (
     LeaderboardRepository,
     get_leaderboard_repository,
@@ -34,44 +36,64 @@ class CreateSnapshot:
         self._player_repo: PlayerRepository = player_repo
 
     async def execute(self, req: CreateSnapshotRequest) -> SnapshotCreatedResponse:
-        snapshot_ids: list[int] = []
+        created_snapshot_ids: list[int] = []
         fetched_at = datetime.now(UTC)
+        unique_names: set[str] = set()
 
         for snapshot_in in req.snapshots:
-            lb = await self._lb_repo.upsert_leaderboard(snapshot_in.leaderboard_name)
-            snapshot_entity = LeaderboardSnapshot(
-                leaderboard_id=lb.id,
-                fetched_at=fetched_at,
-                entries=[
-                    LeaderboardEntry(
-                        rank=entry_in.rank,
-                        player_name=entry_in.player_name,
-                        value=entry_in.value,
-                    )
-                    for entry_in in snapshot_in.entries
-                ],
-            )
-            created_snapshot: LeaderboardSnapshot = await self._lb_repo.create_snapshot(
-                snapshot_entity
-            )
-            snapshot_ids.append(created_snapshot.id)
+            created_snapshot = await self._create_snapshot(snapshot_in, fetched_at)
+            created_snapshot_ids.append(created_snapshot.id)
 
-        unique_names = {
-            entry.player_name
-            for snapshot_in in req.snapshots
-            for entry in snapshot_in.entries
-        }
-
-        for player_name in unique_names:
-            _ = await self._player_repo.upsert_player(player_name)
+            created_players = await self._upsert_players(created_snapshot)
+            unique_names.update(created_players)
 
         await self._unit_of_work.commit()
 
-        for player_name in unique_names:
-            _ = await self._redis.delete(f"player:{player_name.lower()}")
+        for name in unique_names:
+            _ = await self._redis.delete(f"player:{name.lower()}")
 
         return SnapshotCreatedResponse(
-            snapshotIds=snapshot_ids,
+            snapshotIds=created_snapshot_ids,
             fetchedAt=fetched_at,
             message="Snapshots created successfully.",
         )
+
+    async def _create_snapshot(
+        self,
+        snapshot_req: LeaderboardSnapshotIn,
+        fetched_at: datetime,
+    ) -> LeaderboardSnapshot:
+        lb_name = snapshot_req.leaderboard_name
+        lb = await self._lb_repo.get_leaderboard_by_name(lb_name)
+        if lb is None:
+            raise LeaderboardError(
+                LeaderboardErrors.LEADERBOARD_NOT_FOUND,
+                f"Leaderboard {lb_name} does not exist.",
+                {"leaderboardName": lb_name},
+            )
+
+        snapshot_entity = LeaderboardSnapshot(
+            leaderboard_id=lb.id,
+            fetched_at=fetched_at,
+            entries=[
+                LeaderboardEntry(
+                    rank=entry_in.rank,
+                    player_name=entry_in.player_name,
+                    value=entry_in.value,
+                )
+                for entry_in in snapshot_req.entries
+            ],
+        )
+        created_snapshot: LeaderboardSnapshot = await self._lb_repo.create_snapshot(
+            snapshot_entity
+        )
+
+        return created_snapshot
+
+    async def _upsert_players(self, snapshot: LeaderboardSnapshot) -> set[str]:
+        unique_names = set(entry.player_name for entry in snapshot.entries)
+
+        for name in unique_names:
+            _ = await self._player_repo.upsert_player(name)
+
+        return unique_names
---
+++ app/migrations/versions/464057a575d1_recreate_leaderboard_table_with_new_.py
@@ -0,0 +1,78 @@
+"""recreate leaderboard table with new metadata
+
+Revision ID: 464057a575d1
+Revises: 8722c2b5c277
+Create Date: 2026-09-01 05:21:16.061413
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "464057a575d1"
+down_revision: Union[str, Sequence[str], None] = "8722c2b5c277"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    downgrade()  ############################################## REMOVE THIS AFTER DEPLOY ##############################################
+    op.create_table(
+        "leaderboard",
+        sa.Column("id", sa.BigInteger(), nullable=False),
+        sa.Column("external_leaderboard_id", sa.Text(), nullable=False),
+        sa.Column("name", sa.Text(), nullable=False),
+        sa.Column(
+            "estimated_time_per_completion_minutes", sa.Integer(), nullable=False
+        ),
+        sa.Column("group_size", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("external_leaderboard_id"),
+        sa.UniqueConstraint("name"),
+        sa.CheckConstraint(
+            "estimated_time_per_completion_minutes > 0",
+            name="ck_estimated_time_positive",
+        ),
+        sa.CheckConstraint("group_size > 0", "ck_group_size_positive"),
+    )
+
+    op.execute("""
+        CREATE TRIGGER trg_leaderboard_updated
+        BEFORE UPDATE ON leaderboard
+        FOR EACH ROW EXECUTE FUNCTION set_updated_timestamp();
+        """)
+
+    op.execute("""
+        INSERT INTO leaderboard (external_leaderboard_id, name, estimated_time_per_completion_minutes, group_size)
+        VALUES
+        ('Zenith Clears', 'Celestial Zenith', 15, 4),
+        ('Twisted lxxxxxxx Wins', 'Twisted Intruder', 4, 2),
+        ('Aurora Defeats (Caches Claimed)', 'Aurora', 4, 4),
+        ('Hexfall - Hycenea Defeats', 'Hexfall' , 5, 4),
+        ('SKT Savage Mode Clears', 'Silver Knights Tomb', 8, 4),
+        ('Portal', 'Portal', 2, 4),
+        ('MasqueradersRuin', 'Masqueraders Ruin', 7, 6),
+        ('Combat Remnants Looted', 'Silver Knight Remnants (Combat)', 6, 1),
+        ('Puzzle Remnants Looted', 'Silver Knight Remnants (Puzzle)', 8, 1)
+        """)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("DROP TABLE leaderboard CASCADE")
